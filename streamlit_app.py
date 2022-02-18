9d61a0e508c27cc5ffad3b6ca4e3723ec6139fc1
Faith Tosin Olapade <45565979+Faithtosin@users.noreply.github.com>
2022-02-18 10:40:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("🎈 Example app template")
-st.write("Have fun crafting your own example app!")
+st.write("Have fun crafting your own example app! Thanks for coming to my ted talk")
 st.balloons()
